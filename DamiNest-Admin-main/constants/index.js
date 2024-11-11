abdd2e6e15da46a20bee7a74d59ec7210616b4ae
Phuc <phuchoang2411@gmail.com>
2024-11-11 20:14:35 +0700

    remove constant and more

--- DamiNest-Admin-main/constants/index.js
+++
@@ -1,9 +0,0 @@
-const PRODUCTS = require('./product')
-const PRODUCT_CATEGORIES = require('./productCategory')
-const CUSTOMERS = require('./customer')
-
-module.exports = {
-  PRODUCTS,
-  PRODUCT_CATEGORIES,
-  CUSTOMERS
-}
